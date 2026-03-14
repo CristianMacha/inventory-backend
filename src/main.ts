feceb8a6268aef3259e066f939f8fa1528ca12f0
cristianv.ms <c71917455@gmail.com>
2026-03-14 12:34:31 -0500

    chore: Update Node.js engine version in package.json and modify app.listen binding address
    - Added Node.js engine version "20.19.5" to package.json for compatibility.
    - Updated app.listen method to bind to '0.0.0.0' to allow external connections.

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 void bootstrap();
